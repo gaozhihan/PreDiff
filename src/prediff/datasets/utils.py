e1e8f29979eeed8160d67ef9eb37269e2a215962
gaozhihan <gzh93@outlook.com>
2024-09-26 17:14:32 +0800

    Create utils.py

---
+++ src/prediff/datasets/utils.py
@@ -0,0 +1,74 @@
+# Code is adapted from torch.utils.data.dataset
+import warnings
+import math
+from typing import (
+    List,
+    Optional,
+    Sequence,
+    Union
+)
+from torch import default_generator, randperm, Generator
+from torch._utils import _accumulate
+from torch.utils.data import Dataset, Subset
+from torch.utils.data.dataset import T
+
+
+def random_split(dataset: Dataset[T], lengths: Sequence[Union[int, float]],
+                 generator: Optional[Generator] = default_generator,
+                 return_indices: bool = False, ):
+    r"""
+    Randomly split a dataset into non-overlapping new datasets of given lengths.
+
+    If a list of fractions that sum up to 1 is given,
+    the lengths will be computed automatically as
+    floor(frac * len(dataset)) for each fraction provided.
+
+    After computing the lengths, if there are any remainders, 1 count will be
+    distributed in round-robin fashion to the lengths
+    until there are no remainders left.
+
+    Optionally fix the generator for reproducible results, e.g.:
+
+    Example:
+        >>> # xdoctest: +SKIP
+        >>> generator1 = torch.Generator().manual_seed(42)
+        >>> generator2 = torch.Generator().manual_seed(42)
+        >>> random_split(range(10), [3, 7], generator=generator1)
+        >>> random_split(range(30), [0.3, 0.3, 0.4], generator=generator2)
+
+    Args:
+        dataset (Dataset): Dataset to be split
+        lengths (sequence): lengths or fractions of splits to be produced
+        generator (Generator): Generator used for the random permutation.
+    """
+    if math.isclose(sum(lengths), 1) and sum(lengths) <= 1:
+        subset_lengths: List[int] = []
+        for i, frac in enumerate(lengths):
+            if frac < 0 or frac > 1:
+                raise ValueError(f"Fraction at index {i} is not between 0 and 1")
+            n_items_in_split = int(
+                math.floor(len(dataset) * frac)  # type: ignore[arg-type]
+            )
+            subset_lengths.append(n_items_in_split)
+        remainder = len(dataset) - sum(subset_lengths)  # type: ignore[arg-type]
+        # add 1 to all the lengths in round-robin fashion until the remainder is 0
+        for i in range(remainder):
+            idx_to_add_at = i % len(subset_lengths)
+            subset_lengths[idx_to_add_at] += 1
+        lengths = subset_lengths
+        for i, length in enumerate(lengths):
+            if length == 0:
+                warnings.warn(f"Length of split at index {i} is 0. "
+                              f"This might result in an empty dataset.")
+
+    # Cannot verify that dataset is Sized
+    if sum(lengths) != len(dataset):    # type: ignore[arg-type]
+        raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
+
+    indices = randperm(sum(lengths), generator=generator).tolist()  # type: ignore[call-overload]
+    indices_list = [indices[offset - length: offset] for offset, length in zip(_accumulate(lengths), lengths)]
+
+    if return_indices:
+        return [Subset(dataset, ele) for ele in indices_list], indices_list
+    else:
+        return [Subset(dataset, ele) for ele in indices_list]
